88cf1fdd6d7726ecdc64f14f4a1d99a6b49b379a
hold <dd.kuznetsov@avia-centr.ru>
2017-06-28 22:24:12 +0300

    Add meals index spec

--- spec/controllers/meals_controller_spec.rb
+++ spec/controllers/meals_controller_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe MealsController, type: :controller do
+  let(:menu) { create :menu }
+
+  describe '#index' do
+    it 'return meals for current menu' do
+      get :index, menu_id: menu
+      expect(assigns(:meals)).to eq menu.meals
+    end
+  end
 
 end
